f70d39071efeeffe3cc5968aa4e024444db4c4dd
bogdan-404 <bogdan.zlatovcen@gmail.com>
2023-03-22 22:31:43 +0200

    Make changes for new grammar

--- OwnTools/lexer.py
+++ OwnTools/lexer.py
@@ -3,37 +3,33 @@ import re
 tokens = [
     'DESCRIPTION', 'SETTING', 'RESPONSE', 'NAME', 'TYPE', 'MBTI', 'ROLE',
     'BACKGROUND', 'CATEGORY', 'LENGTH', 'PROMPT', 'STRING_LITERAL', 'NUMBER_LITERAL',
-    'LEFT_BRACE', 'RIGHT_BRACE', 'EQUALS', 'LEFT_SQUARE_BRACKET', 'RIGHT_SQUARE_BRACKET', 'COMMA', 'SEMICOLON',
+    'LEFT_BRACE', 'RIGHT_BRACE', 'EQUALS', 'LEFT_SQUARE_BRACKET', 'RIGHT_SQUARE_BRACKET',
 ]
 
-description_regex = r'Description\s*\{'
-setting_regex = r'Setting\s*\{'
-response_regex = r'Response\s*\{'
-name_regex = r'name\s*='
-type_regex = r'type\s*='
-mbti_regex = r'mbti\s*='
-role_regex = r'role\s*='
-background_regex = r'background\s*='
-category_regex = r'category\s*='
-length_regex = r'length\s*='
-prompt_regex = r'prompt\s*='
+description_regex = r'Description\s*'
+setting_regex = r'Setting\s*'
+response_regex = r'Response\s*'
+name_regex = r'name\s*'
+type_regex = r'type\s*'
+mbti_regex = r'mbti\s*'
+role_regex = r'role\s*'
+background_regex = r'background\s*'
+category_regex = r'category\s*'
+length_regex = r'length\s*'
+prompt_regex = r'prompt\s*'
 number_literal_regex = r'\d+'
 left_brace_regex = r'{'
 right_brace_regex = r'}'
 equals_regex = r'='
 left_square_bracket_regex = r'\['
 right_square_bracket_regex = r'\]'
-comma_regex = r','
-semicolon_regex = r';'
-backslash_regex = r'\\'
-string_literal_regex = r'"([^"\\]|\\.)*"|[^;{=}\[\]]+'
+string_literal_regex = r'"([^"\\]|\\.)*"'
 
-# Combine all regular expressions into a single pattern
 token_regex = '|'.join([
     description_regex, setting_regex, response_regex, name_regex, type_regex,
     mbti_regex, role_regex, background_regex, category_regex, length_regex, prompt_regex,
     string_literal_regex, number_literal_regex, left_brace_regex, right_brace_regex,
-    equals_regex, left_square_bracket_regex, right_square_bracket_regex, comma_regex, semicolon_regex, backslash_regex
+    equals_regex, left_square_bracket_regex, right_square_bracket_regex
 ])
 pattern = re.compile(token_regex)
 
@@ -64,11 +60,6 @@ def tokenize(code):
             tokens.append(('LENGTH', None))
         elif re.match(prompt_regex, value):
             tokens.append(('PROMPT', None))
-        elif re.match(string_literal_regex, value):
-            if value.startswith('"') and value.endswith('"'):
-                tokens.append(('STRING_LITERAL', value[1:-1]))
-            else:
-                tokens.append(('STRING_LITERAL', value.strip()))
         elif re.match(number_literal_regex, value):
             tokens.append(('NUMBER_LITERAL', int(value)))
         elif re.match(left_brace_regex, value):
@@ -81,33 +72,31 @@ def tokenize(code):
             tokens.append(('LEFT_SQUARE_BRACKET', None))
         elif re.match(right_square_bracket_regex, value):
             tokens.append(('RIGHT_SQUARE_BRACKET', None))
-        elif re.match(comma_regex, value):
-            tokens.append(('COMMA', None))
-        elif re.match(semicolon_regex, value):
-            tokens.append(('SEMICOLON', None))
-        elif re.match(backslash_regex, value):
-            tokens.append(('BACKSLASH', None))
+        elif re.match(string_literal_regex, value):
+            if value.startswith('"') and value.endswith('"'):
+                tokens.append(('STRING_LITERAL', value[1:-1]))
+            else:
+                tokens.append(('STRING_LITERAL', value.strip()))
     return tokens
 
 
 code = '''
 Description {
-    name=John;
-    type=NPC;
-    mbti=intj;
-    role=protagonist;
-    background=John is a witch hunter;
+    name="John"
+    type="NPC"
+    mbti="intj"
+    role="protagonist"
+    background="John is a witch hunter"
 }
 Setting {
-    type=game;
-    category=adventure, magic, open-world;
-    background=A world similar to Mars, where witches are living in forests and fight with humans;
+    type="game"
+    category="adventure, magic, open-world"
+    background="A world similar to Mars, where witches are living in forests and fight with humans"
 }
 Response {
-    length=300;
-    prompt=I need the background story of the character. Add some info about family. Give 3 possible stories in which John is side character;
-}
-'''
+    length=300
+    prompt="I need the background story of the character. Add some info about family. Give 3 possible stories in which John is side character"
+}'''
 
 tokens = tokenize(code)
 print(tokens)
